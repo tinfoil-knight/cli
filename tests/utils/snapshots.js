1c4419f44828788f67092dd56fa05109213cff8a
erezrokah <erezrokah@users.noreply.github.com>
2021-07-01 14:01:35 +0200

    test: normalize backslash in snapshots

--- tests/utils/snapshots.js
+++ tests/utils/snapshots.js
@@ -1,6 +1,7 @@
 const normalizers = [
   { pattern: /netlify-cli\/.+node-.+/g, value: 'netlify-cli/test-version test-os test-node-version' },
   { pattern: /\d{5}/g, value: '88888' },
+  { pattern: /\\/g, value: '/' },
 ]
 
 const normalize = (inputString) =>
